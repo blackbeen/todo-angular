23b2b684ccad56872a53cfb2c3491535de7ef552
haobin <haobin@citicbank.com>
2017-10-18 20:33:39 +0800

    TODO demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template: `
+  <div>
+    <input type="text" placeholder="Task name here" [(ngModel)]="newTodoText" (keyup.enter)="addTodo()">
+    <input type="submit" value="新增" (click)="addTodo()"/>
+    <ul>
+      <li *ngFor="let todo of todoList; let i=index">
+        <span>{{todo}}</span>
+        <input type="button" value="删除" (click)="removeTodo(todo, i)">
+      </li>
+    </ul>
+  </div>
+  `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  todoList = [];
+  newTodoText = '';
+
+  addTodo(): void {
+    if (this.newTodoText) {
+      this.todoList.push(this.newTodoText)
+      this.newTodoText = ''
+    }
+  };
+
+  removeTodo(item: string, index: number) : void {
+    this.todoList.splice(index, 1);
+  }
 }
